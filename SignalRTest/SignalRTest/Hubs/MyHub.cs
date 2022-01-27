99d5ab9b29a06c6e75ae5e3c5cda53d71b7c5245
fahadli <Farida.Ahadli@kibrit.tech>
2022-01-27 15:04:03 +0400

    signal R test

---
+++ SignalRTest/SignalRTest/Hubs/MyHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.Hubs
+{
+    public class MyHub:Hub
+    {
+        public async void SendMessage(string message)
+        {
+            await Clients.All.SendAsync("message", message);
+        }
+    }
+}
